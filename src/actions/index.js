4c99effe174f92d5296adf0c39a74c4a34cbd752
Latexovich <discord.latex@gmail.com>
2019-10-30 21:55:51 +0300

    move dealers check from getCars to getDealer

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,10 +8,7 @@ export const getCars = (page, per_page) => async dispatch => {
 
     let payload = await api.getCars(page, per_page);
     payload.data = payload.data.map(car => {
-      if (tempArray.indexOf(car.dealer) === -1) {
-        tempArray.push(car.dealer);
-        dispatch(getDealer(car.dealer));
-      }
+      dispatch(getDealer(car.dealer));
       return Object.assign(car, { key: car.vin });
     });
     dispatch({ type: 'GET_CARS_SUCCESS', payload });
@@ -26,7 +23,8 @@ export const getDealer = (id) => async (dispatch, getState) => { //TODO
     const dealers = getState().reducer.get('dealers');
     let dealer = dealers.find(dealer => dealer.id === id);
 
-    if (!dealer) {
+    if (!dealer && tempArray.indexOf(id) === -1) {
+      tempArray.push(id);
       let payload = await api.getDealer(id);
       dispatch({ type: 'GET_DEALER_SUCCESS', payload: payload.data });
     }
